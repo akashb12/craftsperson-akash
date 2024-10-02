c205a07308776739cd51cbe96b3bc4ba414fb991
akashbanaulikar <akashbanaulikar05@gmail.com>
2024-10-02 12:12:30 +0530

    testing environment setup for local testing and test written to return number itself when one number is provided

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,8 +1,19 @@
 class StringCalculator {
-    addition(value) {
-        if(value == "") {
-            return 0;
+    addition(str) {
+        let sum = 0;
+        if (str == "") {
+            return sum;
         }
+        let num = str.split(",").map((num) => {
+            sum += Number(num)
+
+        })
+        return sum
     }
 };
+
+// for testing purpose
+let test = new StringCalculator()
+console.log(test.addition("1,2"));
+
 module.exports = StringCalculator;
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,11 +1,13 @@
 const StringCalculator = require('./stringCalculator');
-describe('StringCalculator' , ()=> {
+describe('StringCalculator', () => {
     let calculateFunc;
     beforeEach(() => {
         calculateFunc = new StringCalculator();
     });
     test('should return 0 for empty string', () => {
-      expect(calculateFunc.addition("")).toBe(0);
+        expect(calculateFunc.addition("")).toBe(0);
     })
-    
+    test('should return number itself when only one number is provided', () => {
+        expect(calculateFunc.addition("1")).toBe(1);
+    })
 })
