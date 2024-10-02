ca6b67171eedf1611b9ab96bb48a1731d98ad968
akashbanaulikar <akashbanaulikar05@gmail.com>
2024-10-02 12:19:38 +0530

    test written to return sum of coma-seperated numbers and with this step 1 is completed

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -4,10 +4,16 @@ describe('StringCalculator', () => {
     beforeEach(() => {
         calculateFunc = new StringCalculator();
     });
+
     test('should return 0 for empty string', () => {
         expect(calculateFunc.addition("")).toBe(0);
-    })
+    });
+
     test('should return number itself when only one number is provided', () => {
         expect(calculateFunc.addition("1")).toBe(1);
+    });
+
+    test('should return sum of two coma-seperated numbers', () => {
+        expect(calculateFunc.addition("1,2")).toBe(3);
     })
 })
